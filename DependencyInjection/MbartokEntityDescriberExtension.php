8ce355acb4e6dd6223c5835f5210b964ce250bd1
mbartok <mbartok@techpoint.sk>
2016-12-28 21:05:10 +0100

    Correcting bundle name

--- DependencyInjection/MbartokEntityDescriberExtension.php
+++ DependencyInjection/MbartokEntityDescriberExtension.php
@@ -3,12 +3,12 @@
 namespace mbartok\EntityDescriberBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Processor;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class EntityDescriberExtension extends Extension
+class MbartokEntityDescriberExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
